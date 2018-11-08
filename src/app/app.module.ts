acc700a56a993cd28e9ae795f13486fac992836f
amsshoyon <amsshoyon@gmail.com>
2018-11-08 11:14:34 +0600

    added some more component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,19 @@ import { AccomodationComponent } from './accomodation/accomodation.component';
 import { RoomsComponent } from './accomodation/rooms/rooms.component';
 import { FoodBeverageComponent } from './food-beverage/food-beverage.component';
 import { RestaurantComponent } from './food-beverage/restaurant/restaurant.component';
+import { BarsComponent } from './food-beverage/bars/bars.component';
+import { LifeStyleComponent } from './life-style/life-style.component';
+import { WelnessAndSpaComponent } from './LifeStyle/welness-and-spa/welness-and-spa.component';
+import { LifsStyleComponent } from './lifs-style/lifs-style.component';
+import { LifestyleComponent } from './lifestyle/lifestyle.component';
+import { ResortActivitiesComponent } from './lifestyle/resort-activities/resort-activities.component';
+import { ExperiencesComponent } from './lifestyle/experiences/experiences.component';
+import { ExcursionsComponent } from './lifestyle/excursions/excursions.component';
+import { WeddingsComponent } from './weddings/weddings.component';
+import { ThaiWeddingsComponent } from './weddings/thai-weddings/thai-weddings.component';
+import { WesternsWeddingsComponent } from './weddings/westerns-weddings/westerns-weddings.component';
+import { CelebrationsComponent } from './celebrations/celebrations.component';
+import { OffersComponent } from './offers/offers.component';
 
 @NgModule({
   declarations: [
@@ -15,7 +28,20 @@ import { RestaurantComponent } from './food-beverage/restaurant/restaurant.compo
     AccomodationComponent,
     RoomsComponent,
     FoodBeverageComponent,
-    RestaurantComponent
+    RestaurantComponent,
+    BarsComponent,
+    LifeStyleComponent,
+    WelnessAndSpaComponent,
+    LifsStyleComponent,
+    LifestyleComponent,
+    ResortActivitiesComponent,
+    ExperiencesComponent,
+    ExcursionsComponent,
+    WeddingsComponent,
+    ThaiWeddingsComponent,
+    WesternsWeddingsComponent,
+    CelebrationsComponent,
+    OffersComponent
   ],
   imports: [
     BrowserModule,
