3d9df0ea780e838c160013c995dc3779897357e4
amsshoyon <amsshoyon@gmail.com>
2018-11-08 10:57:37 +0600

    components created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,18 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import { AccomodationComponent } from './accomodation/accomodation.component';
+import { RoomsComponent } from './accomodation/rooms/rooms.component';
+import { FoodBeverageComponent } from './food-beverage/food-beverage.component';
+import { RestaurantComponent } from './food-beverage/restaurant/restaurant.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AccomodationComponent,
+    RoomsComponent,
+    FoodBeverageComponent,
+    RestaurantComponent
   ],
   imports: [
     BrowserModule,
