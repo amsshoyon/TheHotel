a4b28c4451578f35893d831e0a34cf38c50ae137
amsshoyon <amsshoyon@gmail.com>
2018-11-08 13:58:36 +0600

    added routings

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,7 @@
-import { MaterialModule } from './material.module';
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { AppRoutingModule } from './app-routing.module';
-
-import { AppComponent } from './app.component';
-import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import { RouterModule, Routes } from '@angular/router';
 import { AccomodationComponent } from './accomodation/accomodation.component';
+import { CelebrationsComponent } from './celebrations/celebrations.component';
 import { RoomsComponent } from './accomodation/rooms/rooms.component';
 import { FoodAndBeverageComponent } from './food-and-beverage/food-and-beverage.component';
 import { RestaurantComponent } from './food-and-beverage/restaurant/restaurant.component';
@@ -13,7 +9,6 @@ import { BarsComponent } from './food-and-beverage/bars/bars.component';
 import { WeddingsComponent } from './weddings/weddings.component';
 import { ThaiWeddingsComponent } from './weddings/thai-weddings/thai-weddings.component';
 import { WesternsWeddingsComponent } from './weddings/westerns-weddings/westerns-weddings.component';
-import { CelebrationsComponent } from './celebrations/celebrations.component';
 import { OffersComponent } from './offers/offers.component';
 import { AuthComponent } from './auth/auth.component';
 import { SigninComponent } from './auth/signin/signin.component';
@@ -25,35 +20,28 @@ import { ResortActivitiesComponent } from './lifestyle/resort-activities/resort-
 import { WelnessAndSpaComponent } from './lifestyle/welness-and-spa/welness-and-spa.component';
 
 
+const routes: Routes = [
+    { path: 'Accomodation', component: AccomodationComponent },
+    { path: 'Celebrations', component: CelebrationsComponent },
+    { path: 'Rooms', component: RoomsComponent },
+    { path: 'Food&Beverage', component: FoodAndBeverageComponent },
+    { path: 'Restaurant', component: RestaurantComponent },
+    { path: 'Bars', component: BarsComponent },
+    { path: 'Weddings', component: WeddingsComponent },
+    { path: 'ThaiWeddings', component: ThaiWeddingsComponent },
+    { path: 'WesternsWeddings', component: WesternsWeddingsComponent },
+    { path: 'Offers', component: OffersComponent },
+    { path: 'Auth', component: AuthComponent },
+    { path: 'SignIn', component: SigninComponent },
+    { path: 'SignUp', component: SignupComponent },
+    { path: 'LifeStyles', component: LifestyleComponent },
+    { path: 'Experiences', component: ExperiencesComponent },
+    { path: 'Excursions', component: ExcursionsComponent },
+    { path: 'Resort', component: ResortActivitiesComponent },
+    { path: 'Welness$Spa', component: WelnessAndSpaComponent }
+];
 @NgModule({
-  declarations: [
-    AppComponent,
-    AccomodationComponent,
-    RoomsComponent,
-    FoodAndBeverageComponent,
-    RestaurantComponent,
-    BarsComponent,
-    WeddingsComponent,
-    ThaiWeddingsComponent,
-    WesternsWeddingsComponent,
-    CelebrationsComponent,
-    OffersComponent,
-    AuthComponent,
-    SigninComponent,
-    SignupComponent,
-    LifestyleComponent,
-    ExperiencesComponent,
-    ExcursionsComponent,
-    ResortActivitiesComponent,
-    WelnessAndSpaComponent
-  ],
-  imports: [
-    BrowserModule,
-    BrowserAnimationsModule,
-    MaterialModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule]
 })
-export class AppModule { }
+export class AppRoutingModule {}
